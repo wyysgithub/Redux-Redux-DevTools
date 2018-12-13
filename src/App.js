8b1481525d6997a69e4312ef2f98c0108487116e
wangyangyang <gitlab123456>
2018-12-13 17:45:08 +0800

    整合React

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {connect} from 'react-redux';
+
+
+
 import './App.css';
+import {ADD_TO_CART, addCart} from "./actions/cart-action";
+import store from "./store";
+
+// const mapStateToProps = (state) => {
+//     return {
+//         cart: state.cart.cart
+//     }
+// }
+// const mapDispatchToProps = (dispatch) =>{
+//    return{
+//      onClick:() =>{
+//        dispatch({
+//            type:ADD_TO_CART,
+//            product:'li',
+//            quantity:3
+//        })
+//      }
+//    }
+// }
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={}
+  }
+  componentDidMount(){
+    store.subscribe((state)=> this.setState(state))
+    console.log(this.props)
+    console.log(this.state)
+  }
+  addTo(){
+    store.dispatch(addCart('a',2))
+      console.log(store.getState())
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          {store.getState().cart.cart.map((item,i)=>{
+                return(
+                    <div key={i}>
+                        {item.product}
+                    </div>
+                )
+          })}
+          <button onClick={this.addTo}>add</button>
       </div>
     );
   }
 }
-
+// App = connect(mapStateToProps,mapDispatchToProps)(App);
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,8 +5,25 @@
 import store from './store'
 import {addCart,updateCart,deleteCart} from "./actions/cart-action";
 
-console.log(store.getState())
+/**
+ * 集成react
+ */
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App';
 
+// const App = <h1>这是一段定义在index里的话</h1>;
+
+ReactDom.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+)
+
+
+//监听状态
 let unsubscribe = store.subscribe(()=>{
     console.log(store.getState())
 })
